f4a53d97964f861530ef943928f4072e42eaf067
Enmanuel1395 <anguloenmanuel2@gmail.com>
2025-02-28 19:04:55 +0000

    peimer commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let whoRandom = Math.floor(Math.random() * who.length);
+  let actionRandom = Math.floor(Math.random() * action.length);
+  let thatRandom = Math.floor(Math.random() * what.length);
+  let whenRandom = Math.floor(Math.random() * when.length);
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+  document.getElementById("excuse").innerHTML = `${who[whoRandom]} ${action[actionRandom]} ${what[thatRandom]} ${when[whenRandom]}`
+  };
